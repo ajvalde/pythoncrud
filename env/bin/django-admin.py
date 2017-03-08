29a347728dfb273a58c6faf72f3ba9fed54b7aad
Anthony <anthonyguitarman13@gmail.com>
2017-03-08 09:16:33 -0700

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/anthony/projects/quater-3/foriegn/pythonstuff/pythonapp/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
