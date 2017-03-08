67bcbbaf3cf4e221189807753aa69ebcac23d84e
Anthony <anthonyguitarman13@gmail.com>
2017-03-08 12:25:00 -0700

    hello world

--- rememberme/todo/models.py
+++ rememberme/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'DRAFT'),
+        ('published', 'Published'),
+    )
+    name = models.CharField(max_length=250)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- rememberme/todo/views.py
+++ rememberme/todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def list_of_post(request):
+    post = Post.objects.all()
+    template = 'list_of_posts.html'
+    context = {'post': post}
+    return render(request, template, context)
